7565d29dc0d35fa2164c0fc80ab42ffb591d2760
Stefano Balietti <futur.dorko@gmail.com>
2014-02-25 15:20:56 +0100

    minor

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /**
- * # nodeGame-mongodb
- * 
+ * # nodegame-mongodb
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed 
+ *
  * MongoDB component for nodeGame
  * 
  * http://www.nodegame.org
- * 
  * ---
- * 
  */
-
 module.exports.NGM = require('./lib/MongoLayer.js');
